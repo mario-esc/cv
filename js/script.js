28a816b318d13fca709defa6a421dad51a0dd6e4
Mario Escobar <mario.h.esc@gmail.com>
2023-01-18 02:22:22 -0300

    Tercer commit versión - Se finalizó la carga de datos del CV. Se pueden generar usuarios aleatorios con los que cambiarán ciertos elementos del html (Foto de perfil, nombre y apellido, y datos personales)

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
 
-
-console.log('hola mundirijillo');
-
-/* fetch('https://randomuser.me/api/')
-.then(response => response.json())
-.then(data => console.log(data)); */
-
-
-
-
-
-
+/* Inicio inicialización de variables basadas en IDs */
 const generatorButton = document.getElementById('generator-button');
 const profilePhoto = document.getElementById('profile-photo');
 const lastName = document.getElementById('last-name');
@@ -21,36 +10,39 @@ const behanceUser = document.getElementById('behance-user');
 const phoneUser = document.getElementById('phone-user');
 const mailUser = document.getElementById('mail-user');
 const userProfession = document.getElementById('profession');
+/* Fin inicialización de variables basadas en IDs */
 
+/* Función para conseguir el género del usuario desde la appi, y asignarle la profesión */
+function userProfessionBasedOnGender(gender) {
+        
+    if (gender == 'female') {
+        return 'Diseñadora UX/UI';
+    } else {
+        return 'Diseñador UX/UI';
+    }
+};
+/* Fin función género del usuario */
+
+/* Inicio inicialización de variables basadas en IDs */
 const userGenerator = async function () {
+    /* Consiguiendo datos de la api  */
     const urlApi = 'https://randomuser.me/api/?inc=gender,name,cell,email,login,picture';
     const user = await fetch(urlApi);
     const { results } = await user.json();
     const userData = results[0];
     
+    /* Completando info en el html  */
     profilePhoto.src = userData.picture.large;
     lastName.textContent = userData.name.last;
     firstName.textContent = userData.name.first;
-    
-    let userGender = 0;
-        
-    if (userData.gender == 'female') {
-        userGender = 'Programadora';
-        } else {
-            userGender = 'Programador'
-        };
-
-    userProfession.textContent = userGender;
+    userProfession.textContent = userProfessionBasedOnGender(userData.gender);
 
     linkedinUser.textContent = userData.login.username.slice(0,6);
     behanceUser.textContent = userData.login.username.slice(0,6);
     phoneUser.textContent = userData.cell;
     mailUser.textContent = userData.email.slice(0,8)+'@gmail.com';
 
-    
-
-    console.log(userData);
-    console.log('generando usuario');
+    /* console.log(userData); */
 };
 
 generatorButton.addEventListener('click', userGenerator);
@@ -58,21 +50,7 @@ document.addEventListener('DOMContentLoaded', userGenerator);
 
 
 
-/* async function persona(){
-    let resultado = await fetch('https://randomuser.me/api/?inc=name,cell,email,login,picture')
-                            .then(response => response.json())
-                            .then(data => console.log(data));
-    console.log(resultado)
-}
-
-persona(); */
-
-
-
-
-
 /* Inicio despliegue de menus */
-
 const contactButton = document.getElementById('contact-button');
 const educationButton = document.getElementById('education-button');
 /* let userContact = document.querySelector('.user-contact'); */
@@ -91,5 +69,4 @@ educationButton.addEventListener("click", function(){
     educationInfo.classList.toggle('d-vis');
     arrowEducation.classList.toggle('fa-arrow-up');
 })
-
 /* Fin despliegue de menus */
