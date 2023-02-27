722771387afb86c76272cdb6c4b92e1ab1cf11ad
mario-esc <mario.h.esc@gmail.com>
2023-02-26 21:42:13 -0300

    Se agregó un botón que vuelva al top, y se trabajó en el código para el despliegue de menues

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 /* Inicio inicialización de variables basadas en IDs */
 const generatorButton = document.getElementById('generator-button');
 const profilePhoto = document.getElementById('profile-photo');
@@ -14,59 +13,63 @@ const userProfession = document.getElementById('profession');
 
 /* Función para conseguir el género del usuario desde la appi, y asignarle la profesión */
 function userProfessionBasedOnGender(gender) {
-        
-    if (gender == 'female') {
-        return 'Diseñadora UX/UI';
-    } else {
-        return 'Diseñador UX/UI';
-    }
-};
+  if (gender == 'female') {
+    return 'Diseñadora UX/UI';
+  } else {
+    return 'Diseñador UX/UI';
+  }
+}
 /* Fin función género del usuario */
 
 /* Inicio inicialización de variables basadas en IDs */
 const userGenerator = async function () {
-    /* Consiguiendo datos de la api  */
-    const urlApi = 'https://randomuser.me/api/?inc=gender,name,cell,email,login,picture';
-    const user = await fetch(urlApi);
-    const { results } = await user.json();
-    const userData = results[0];
-    
-    /* Completando info en el html  */
-    profilePhoto.src = userData.picture.large;
-    lastName.textContent = userData.name.last;
-    firstName.textContent = userData.name.first;
-    userProfession.textContent = userProfessionBasedOnGender(userData.gender);
+  /* Consiguiendo datos de la api  */
+  const urlApi =
+    'https://randomuser.me/api/?inc=gender,name,cell,email,login,picture';
+  const user = await fetch(urlApi);
+  const { results } = await user.json();
+  const userData = results[0];
 
-    linkedinUser.textContent = userData.login.username.slice(0,6);
-    behanceUser.textContent = userData.login.username.slice(0,6);
-    phoneUser.textContent = userData.cell;
-    mailUser.textContent = userData.email.slice(0,8)+'@gmail.com';
+  /* Completando info en el html  */
+  profilePhoto.src = userData.picture.large;
+  lastName.textContent = userData.name.last;
+  firstName.textContent = userData.name.first;
+  userProfession.textContent = userProfessionBasedOnGender(userData.gender);
 
-    /* console.log(userData); */
+  linkedinUser.textContent = userData.login.username.slice(0, 6);
+  behanceUser.textContent = userData.login.username.slice(0, 6);
+  phoneUser.textContent = userData.cell;
+  mailUser.textContent = userData.email.slice(0, 8) + '@gmail.com';
+
+  /* console.log(userData); */
 };
 
 generatorButton.addEventListener('click', userGenerator);
 document.addEventListener('DOMContentLoaded', userGenerator);
 
-
-
 /* Inicio despliegue de menus */
-const contactButton = document.getElementById('contact-button');
-const educationButton = document.getElementById('education-button');
-/* let userContact = document.querySelector('.user-contact'); */
-const userContact = document.getElementById('user-contact');
-const educationInfo = document.getElementById('education-info');
+const titleClick = document.querySelectorAll('.title-click');
 
-const arrowContact = document.getElementById('arrow-contact');
-const arrowEducation = document.getElementById('arrow-education');
+titleClick.forEach((elementClick) => {
+  elementClick.addEventListener('click', () => {
+    let menu = elementClick.nextElementSibling;
+    menu.classList.toggle('d-vis');
 
-contactButton.addEventListener("click", function(){
-    userContact.classList.toggle('d-vis');
-    arrowContact.classList.toggle('fa-arrow-up');
-})
+    let arrowClicked = elementClick.childNodes[1].childNodes[0];
+    console.log(arrowClicked);
+    arrowClicked.classList.toggle('fa-arrow-up');
+  });
+});
+/* Fin despliegue de menus */
 
-educationButton.addEventListener("click", function(){
-    educationInfo.classList.toggle('d-vis');
-    arrowEducation.classList.toggle('fa-arrow-up');
-})
+//Botón a top
+const toTop = document.querySelector('.to-top');
+
+toTop.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
 /* Fin despliegue de menus */
